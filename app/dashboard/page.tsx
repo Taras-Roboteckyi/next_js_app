e93ec32f4dda9a85dd5689f9e29fadd25f6213be
Taras-Roboteckyi <rtvr@ukr.net>
2023-10-18 21:34:20 +0300

    test4

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-const dashboard = () => {
-  return <h2>Dashboard page</h2>;
-};
-
-export default dashboard;
--- app/dashboard/setting/page.tsx
+++
@@ -1,5 +0,0 @@
-const test = () => {
-  return <h2>Settings page</h2>;
-};
-
-export default test;
