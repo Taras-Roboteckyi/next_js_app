2dd57e4f3b62abc02ddaf31841dd52e8c3d5e3bf
Taras-Roboteckyi <rtvr@ukr.net>
2023-10-26 21:59:22 +0300

    add ISR method

--- app/info.js
+++ app/info.js
@@ -14,13 +14,30 @@ async function Page({ params }) {
   return <h1>{data.body}</h1>;
 }
 
-//2. Static Site Generating (SSG)
+//2. Static Site Generating (SSG). Next.js використовує це по дефолту.
 
 async function Page({ params }) {
   const res = await fetch(
     `http://jsonplacehilder.typicode.com/posts/${params.id}`
   );
   //next.js за замовчуванням кешує дані
+
+  const data = res.json();
+  return <h1>{data.body}</h1>;
+}
+
+//3. Incremental Static Generation (ISR).
+
+async function Page({ params }) {
+  const res = await fetch(
+    `http://jsonplacehilder.typicode.com/posts/${params.id}`
+  );
+  //Робить наступну повторну перевірку через час 10.Тобто кешує дані і через проміжок часу -10 оновлює його. Поєднює в собі два методи SSR та SSG.
+  {
+    next: {
+      revalidate: 10;
+    }
+  }
 
   const data = res.json();
   return <h1>{data.body}</h1>;
