eebb4ac6b5797e92c97d6c6c17bea4837ccbc299
Taras-Roboteckyi <rtvr@ukr.net>
2023-11-08 22:42:07 +0200

    added page component in app

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,22 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section
+      className="w-full flex-center flex-col" /* //w-full - width:100% */
+    >
+      <h1
+        className="head_text text-center" /* //head_text-взято з global.css */
+      >
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
