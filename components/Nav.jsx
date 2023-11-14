43497214faea333b849275b9aaceb7369ff60242
Taras-Roboteckyi <rtvr@ukr.net>
2023-11-14 23:23:24 +0200

    added mobile navigation in Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,20 +4,51 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"; //Робить процес реєстрації клієнта дуже простим
+
 import Logo from "../public/assets/images/logo.svg";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          scr={Logo}
+          src={Logo}
           alt="Promptopia Logo"
           width={30}
           height={30}
           className="object-contain"
         />
+        <p className="logo_text">Promptopia</p>
       </Link>
+
+      {/* Mobile navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src={Logo}
+                alt="profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              ></Image>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
