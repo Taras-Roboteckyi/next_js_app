59bc748468260c1cfb18bea4bcb5a97f9e8a9d84
Taras-Roboteckyi <rtvr@ukr.net>
2023-11-30 22:12:28 +0200

    connected to mongoDB Atlas

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 //Ховаєм наші дані google cloud сховища в env файлі
 const handler = NextAuth({
   providers: [
@@ -13,7 +15,14 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       //serverless => lambda => dynamodb
-    } catch (error) {}
+      await connectToDB();
+      //тепер треба зробити перевірку чи існує уже користувач
+      // якщо користувач не існує, тоді створити нового користувача та зберегти в базу даних
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
