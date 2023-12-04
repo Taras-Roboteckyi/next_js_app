7f0e022a5cb564c7a77292b13c20c2462d6a04ad
Taras-Roboteckyi <rtvr@ukr.net>
2023-12-04 22:45:06 +0200

    added coments to models shema

--- models/user.js
+++ models/user.js
@@ -18,3 +18,13 @@ const userShema = new Schema({
     type: String,
   },
 });
+// the "models" object is provided be the Mongoose library and stores all the registered models.
+// If a model named "User" already exists in the "models" object, it assigns that existing model to the "User" variable.
+// This prevents redefining the model and ensures that the existing model is reused.
+
+//If a model named "User" does not exist in the "models" object, the "model" function from Mongoose is called to create a new model
+// The newly created model is then assigned to the "User" variable.
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
