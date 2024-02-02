c6452bc34d6e2d74adac186fd9d2243f786fbc50
Taras-Roboteckyi <rtvr@ukr.net>
2024-02-02 22:52:08 +0200

    added markup in form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -32,6 +32,28 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className="form_textarea"
           ></textarea>
         </label>
+
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {` `} {/* -   так робим пробіл в тексті. Корисно! */}
+            <span className="font-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+
+          <input
+            value={post.tag}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                tag: e.target.value,
+              })
+            }
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
       </form>
     </section>
   );
