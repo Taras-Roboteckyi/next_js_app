65f717560e841916c20f05e883dd1db80b4b81da
Taras-Roboteckyi <rtvr@ukr.net>
2024-02-08 23:11:49 +0200

    added  import connectToDb

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,3 +1,10 @@
+import { connectToDB } from "@utils/database";
+
 export const POST = async (req, res) => {
   const { userId, prompt, tag } = await req.json();
+
+  try {
+    //Підключаємось до бази даних
+    await connectToDB();
+  } catch (error) {}
 };
