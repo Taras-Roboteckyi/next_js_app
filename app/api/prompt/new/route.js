adb249c51212a81eeacedabc30823999e30041b2
Taras-Roboteckyi <rtvr@ukr.net>
2024-02-12 22:29:55 +0200

    created new function for route'prompt/new/route'

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,23 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
+//Створюєм маршрут в next.js
 export const POST = async (req, res) => {
-  const { userId, prompt, tag } = await req.json();
+  const { userId, prompt, tag } = await req.json(); //витягуєм дані які ми передали через POST запит
 
   try {
     //Підключаємось до бази даних
     await connectToDB();
+    //Cтворюєм новий Prompt
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+    //Викликаєм новий Prompt і зберігаєм в базі даних
+    await newPrompt.save();
+
+    //Тепер повертаєм нову відповідь і статус 201
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
   } catch (error) {}
 };
