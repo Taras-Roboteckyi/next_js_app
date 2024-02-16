d042716d84ce399529ac4a0d45d869acf955865d
Taras-Roboteckyi <rtvr@ukr.net>
2024-02-16 22:26:21 +0200

    added atributes and functions to Feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,9 +5,22 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e) => {};
+
   return (
     <section className="feed">
-      <form className="relative w-full flex-center"></form>
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
     </section>
   );
 };
