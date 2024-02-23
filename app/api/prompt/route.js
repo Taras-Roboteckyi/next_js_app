19d8e1d368b535abb65b01e37f4a0f0d56bcdd07
Taras-Roboteckyi <rtvr@ukr.net>
2024-02-23 23:00:37 +0200

    filled in route GET

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,4 +2,15 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 //Створюєм маршрут в next.js
-export const GET = async (request) => {};
+export const GET = async (request) => {
+  try {
+    //Підключаємось до бази даних
+    await connectToDB();
+    //тепер фільтруєм наші prompts
+    const prompts = await Prompt.find({}).populate("creator");
+    //Повертаєм нову відповідь і статус 200
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
