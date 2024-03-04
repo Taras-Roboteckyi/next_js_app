aa7af3b69394da347600c78d180e76ebf38ed3c0
Taras-Roboteckyi <rtvr@ukr.net>
2024-03-04 22:39:11 +0200

    added function handleCopy

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,7 +8,11 @@ import { usePathname, useRouter } from "next/navigation";
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
 
-  const handleCopy = () => {};
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  };
 
   return (
     <div className="prompt_card">
