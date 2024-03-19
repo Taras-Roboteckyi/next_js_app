3aebaa1b2525f7ec95039580b351c1551b6b5eb7
Taras-Roboteckyi <rtvr@ukr.net>
2024-03-19 23:24:25 +0200

    create PATCH request in Prompt route

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,7 +1,9 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-//Створюєм маршрут в next.js
+//Створюєм маршрути в next.js
+///GET
+
 export const GET = async (request, { params }) => {
   try {
     //Підключаємось до бази даних
@@ -11,11 +13,40 @@ export const GET = async (request, { params }) => {
     const prompt = await Prompt.findById(params.id).populate("creator");
 
     //Робимо перевірку на наявність prompta
-    if (!prompt) new Response("Prompt not found", { status: 404 });
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
 
     //Повертаєм нову відповідь і статус 200
     return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
+
+///Patch (update)
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    //Підключаємось до бази даних
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    //Робимо перевірку на наявність існуючого prompta
+    if (!existingPrompt)
+      return new Response("Prompt not found", { status: 404 });
+
+    //Оновлюємо дані
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    //Зберігаєм оновлені дані
+    await existingPrompt.save();
+
+    //Повертаєм нову відповідь і статус 200
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 500 });
   }
 };
