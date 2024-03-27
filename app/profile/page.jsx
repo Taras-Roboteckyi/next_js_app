0deaef960318b15a2e3286d8a0b788e8dc3a8905
Taras-Roboteckyi <rtvr@ukr.net>
2024-03-27 22:41:54 +0200

    added functionality in handleDelete function

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -32,10 +32,26 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post.id}`);
   };
 
-  const handleDelete = async () => {
+  const handleDelete = async (post) => {
     const hasConfirmed = confirm(
       "Are you sure you want to delete this prompt?"
     );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => {
+          p._id !== post._id;
+        });
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
