7a1c339fe4ac3565f4c465ea62f9a717d8ffa9dc
Taras-Roboteckyi <rtvr@ukr.net>
2024-04-02 22:20:44 +0300

    added function filterPrompts on Feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -38,6 +38,16 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
+  //Фільтруємо Prompts на основі цих запитів
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // "i" - flag for case-insensitive search
+    return allPosts.filter((item) => {
+      regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt);
+    });
+  };
+
   const handleSearchChange = (e) => {};
 
   return (
