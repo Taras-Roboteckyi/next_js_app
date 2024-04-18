500ef45b94c9a1345c75faddc288925fd47201d2
Taras-Roboteckyi <rtvr@ukr.net>
2024-04-18 23:15:04 +0300

    created new page - app/profile/[id]/page.jsx

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  const [userPosts, setUserPosts] = useState([]);
+};
+
+export default UserProfile;
