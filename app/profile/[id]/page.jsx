8169e2eecacd9de17ec136b4ef95ec21ec5ea06c
Taras-Roboteckyi <rtvr@ukr.net>
2024-04-19 22:31:18 +0300

    added UseEffect and fetch request in profile/id/page

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -10,6 +10,17 @@ const UserProfile = ({ params }) => {
   const userName = searchParams.get("name");
 
   const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    };
+
+    if (params?.id) fetchPosts();
+  }, [params.id]);
 };
 
 export default UserProfile;
