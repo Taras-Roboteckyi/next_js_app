dd3c05778b7c8162713e4b7c6291f0d80df65f72
Taras-Roboteckyi <rtvr@ukr.net>
2024-04-24 22:33:35 +0300

    added data in Profile component

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -26,6 +26,7 @@ const UserProfile = ({ params }) => {
     <Profile
       name={userName}
       desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+      data={userPosts}
     />
   );
 };
