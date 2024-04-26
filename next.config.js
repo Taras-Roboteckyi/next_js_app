028fa0ee47e2ad0597dbef258f3c4e08258edfb0
Taras-Roboteckyi <rtvr@ukr.net>
2024-04-26 22:58:54 +0300

    changed next config settings

--- next.config.js
+++ next.config.js
@@ -2,13 +2,19 @@
 /* const nextConfig = {}; */
 
 const nextConfig = {
+  basePath: "/promptopia_next_js_app",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+
   webpack(config) {
     config.experiments = {
       ...config.experiments,
